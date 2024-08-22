86525376037f65d56338aefd1d3045b184ae9c43
AnkeVanCampen <152158859+AnkeVanCampen@users.noreply.github.com>
2024-08-22 11:11:28 +0200

    Add files via upload

--- toyJSS _v3.py
+++ toyJSS _v3.py
@@ -130,7 +130,7 @@ max_makespan = 0
 for m in range(machines_count):
     idx = np.where(sequence_data == m)
     max_makespan += sum(parts_matrix[idx] * machining_data[m])
-max_makespan = 1000*max_makespan
+max_makespan = 10000*max_makespan
 
 
 sum_of_tasks = 0
@@ -178,7 +178,8 @@ flow_operator2task = intvar(0, range_operators[-1], shape=(sum_of_tasks))
 
 knowledge = intvar(1,3,shape=(sum_of_tasks))
 preference = intvar(1,3,shape=(sum_of_tasks))
-execution_time = intvar(2,20,shape=(sum_of_tasks))
+execution_time = intvar(2,100,shape=(sum_of_tasks))
+penalty_time = intvar(0,100,shape=(sum_of_tasks))
 
 
 """----------------------------------------------------------------------------
@@ -188,9 +189,9 @@ ub_ergo = 3
 lb_allocation4operator = int(round(0.7 * sum_of_tasks/operators_count))
 ub_allocation4operator = int(round(1.3 * sum_of_tasks/operators_count))
 ub_dur = 100
-weight_preference = 1000
-weight_expertlevel = 1
-weight_makespan = 0
+weight_preference = 100
+weight_expertlevel = 10
+weight_makespan = 1
 batch_size = 5
 #introduce a scaling factor for the ub of the makespan.
 #why? Actual problem has a max makespan of 10 days; this toy example does not have a max makespan
@@ -198,6 +199,10 @@ batch_size = 5
 ub_scaler = 0.5    
 ub_makespan = int(ub_scaler * max_makespan)
 ub_workscheme = 100
+
+pen_switch = 10 #operator moves between machines
+pen_ergo = 20 #operator needs to take a pause
+pen_general = 0 #buffer time
 """----------------------------------------------------------------------------
 ##-modelling
 """
@@ -225,12 +230,8 @@ for i in range_tasks:
             model += (((flow_machine2task[i] == m) & (flow_operator2task[i] == o)).implies(knowledge[i] == operator_knowl[o,m]))
             model += (((flow_machine2task[i] == m) & (flow_operator2task[i] == o) & (knowledge[i] == operator_knowl[o,m])).implies(
                 (execution_time[i] == machining_data[m,(operator_knowl[o,m]-1)])))
-    model += (start_time[i] + execution_time[i] == end_time[i])       
+    model += (start_time[i] + execution_time[i] + penalty_time[i] == end_time[i])       
 
-##NOT VALID#
-#for i1,i2 in combinations(range_tasks, 2):
-#        model += (flow_machine2task[i1] == flow_machine2task[i2]).implies(Xor([(end_time[i1] <= start_time[i2]),(end_time[i2] <= start_time[i1])]))
-############
 
 ### SLOW CONSTRAINT for many jobs
 for m in range_machines:
@@ -240,15 +241,13 @@ for m in range_machines:
         idx1 = mach[i1]
         idx2 = mach[i2]
         model += (Xor([(end_time[idx1] <= start_time[idx2]),(end_time[idx2] <= start_time[idx1])]))
-        #model += ((end_time[idx1] <= start_time[idx2]) | (end_time[idx2] <= start_time[idx1]))
-        #model += ((start_time[idx2] >= start_time[idx1] + execution_time[idx1]) | (start_time[idx1] >= start_time[idx2] + execution_time[idx2]))
 
-### SLOW CONSTRAINT for many jobs
+# ### SLOW CONSTRAINT for many jobs
 for o1,o2 in combinations(range_tasks,2):
 # # an operator cannot work on different tasks at the same time
-     model += ((flow_operator2task[o1] == flow_operator2task[o2]).implies(
-             Xor([end_time[o1]  <= start_time[o2],end_time[o2] <= start_time[o1]])
-             ))
+      model += ((flow_operator2task[o1] == flow_operator2task[o2]).implies(
+              Xor([end_time[o1]  <= start_time[o2],end_time[o2] <= start_time[o1]])
+              ))
           
 for o in range_operators:
     #each operator should be allocated to a min and max set of tasks
@@ -269,20 +268,111 @@ for r in range(0,len(parts_matrix[:,0]),1):
                 model += (start_time[i] >= end_time[i-int(parts_matrix[r,c-1])])
         if c == (len(parts_matrix[0,:])-1):
             countParts += parts_matrix[r,c]
+
+  
+for t1, t2 in combinations(range_tasks, 2):
+    #operator switches machines: assign penalty time
+    """
+    ### implementation OPTION 1: add penalty to execution time
+    model += (((end_time[t1] == start_time[t2])  & \
+              (flow_operator2task[t1] == flow_operator2task[t2]) & \
+              (machine2task[t1] != machine2task[t2])).implies(execution_time[t1] == execution_time[t1] + pen_discontinu))#implies(penalty_time[t1] == pen_discontinu))
+    model += (((end_time[t2] == start_time[t1])  & \
+              (flow_operator2task[t1] == flow_operator2task[t2]) & \
+              (machine2task[t1] != machine2task[t2])).implies(execution_time[t2] == execution_time[t2] + pen_discontinu))#implies(penalty_time[t2] == pen_discontinu))
+    """
+
+    ### implementation OPTION 2: add penalty time to end time
+    model += (((end_time[t1] == start_time[t2])  & \
+              (flow_operator2task[t1] == flow_operator2task[t2]) & \
+              (machine2task[t1] != machine2task[t2])).implies(penalty_time[t1] == pen_switch))
+    model += (((end_time[t2] == start_time[t1])  & \
+              (flow_operator2task[t1] == flow_operator2task[t2]) & \
+              (machine2task[t1] != machine2task[t2])).implies(penalty_time[t2] == pen_switch))
+    #operator does not switch machines: pen time == 0    
+    model += (((end_time[t2] != start_time[t1]) | (end_time[t1] != start_time[t2]) | \
+                (flow_operator2task[t1] != flow_operator2task[t2])).implies(penalty_time[t2] == pen_general))
+    model += (((end_time[t2] != start_time[t1]) | (end_time[t1] != start_time[t2]) | \
+                (flow_operator2task[t1] != flow_operator2task[t2])).implies(penalty_time[t1] == pen_general))      
+        
+
+        
+for m in range_machines:
+###operator needs pause after many execution of the same task
+    mach =  np.where(flow_machine2task == m)[0]
+    for i1,i2,i3 in combinations(range(len(mach)), 3):
+        idx1 = mach[i1]
+        idx2 = mach[i2]
+        idx3 = mach[i3]
+        
+        """
+        ### implementation OPTION 1: add penalty to execution time
+        model += (((((start_time[i3] == end_time[i2]) & (start_time[i2] == end_time[i1])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i3] == end_time[i1]) & (start_time[i1] == end_time[i2])) & \
+                      ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(execution_time[i3] == execution_time[i3] + pen_ergo)#implies(penalty_time[i3] == pen_ergo)
+                  )
+        model += (((((start_time[i2] == end_time[i1]) & (start_time[i1] == end_time[i3])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i2] == end_time[i3]) & (start_time[i3] == end_time[i1])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(execution_time[i2] == execution_time[i2] + pen_ergo)#(penalty_time[i2] == pen_ergo)
+                  )
+        model += (((((start_time[i1] == end_time[i2]) & (start_time[i2] == end_time[i3])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i1] == end_time[i3]) & (start_time[i3] == end_time[i2])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(execution_time[i1] == execution_time[i1] + pen_ergo)#(penalty_time[i1] == pen_ergo)
+                  )    
+        """
+        ### implementation OPTION 2: add penalty time to end time
+        model += (((((start_time[i3] == end_time[i2]) & (start_time[i2] == end_time[i1])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i3] == end_time[i1]) & (start_time[i1] == end_time[i2])) & \
+                      ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(penalty_time[i3] == pen_ergo)
+                  )
+        model += (((((start_time[i2] == end_time[i1]) & (start_time[i1] == end_time[i3])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i2] == end_time[i3]) & (start_time[i3] == end_time[i1])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(penalty_time[i2] == pen_ergo)
+                  )
+        model += (((((start_time[i1] == end_time[i2]) & (start_time[i2] == end_time[i3])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1]))) | \
+                    (((start_time[i1] == end_time[i3]) & (start_time[i3] == end_time[i2])) & \
+                    ((flow_operator2task[i3] == flow_operator2task[i2]) & (flow_operator2task[i3] == flow_operator2task[i1])))).implies(penalty_time[i1] == pen_ergo)
+                  ) 
+         
+            
 goal_makespan = weight_makespan * max(end_time)    
 goal_knowl = weight_expertlevel * sum(knowledge)
 goal_pref = weight_preference * sum(preference)
-model.minimize(goal_knowl+goal_pref+goal_makespan)
+model.minimize(-goal_knowl-goal_pref+goal_makespan)
 print('================SOLVING CP====================')
 val = model.solve()    
 print(val)
-print("knowledge: ", knowledge.value())
-print("preference: ",  preference.value())
-print('start time: ',  start_time.value())
-print('end time: ',  end_time.value())
-print('execution time: ',  execution_time.value())
-print('operator flow: ', flow_operator2task.value())
-print('machine flow: ', machine2task)
+
+for o in range_operators: 
+    idxop = np.where(flow_operator2task.value() == o)[0]
+    data = start_time.value()[idxop]
+    idxsort = np.argsort(data)
+    print("------------------------------------")
+    print("SCHEDULE for OPERTATOR ", o , " : ")
+    data = machine2task[idxop]
+    print('machine flow: ',  data[idxsort])
+    data = start_time.value()[idxop]
+    print('start time: ',  data[idxsort])
+    data = end_time.value()[idxop]
+    print('end time: ',  data[idxsort])
+    data = execution_time.value()[idxop]
+    print('execution time: ',  data[idxsort])
+    data = penalty_time.value()[idxop]
+    print('penalty time: ',  data[idxsort])
+    data = knowledge.value()[idxop]
+    print("knowledge: ", data[idxsort])
+    data = preference.value()[idxop]
+    print("preference: ",  data[idxsort])
+    
+    print("------------------------------------")
+    
+
 print('#######################################################################################################')
 print('Script was validated up to here: PLEASE NOTE THAT CONSTRAINT SET IS INCOMPLETE')
 print('#######################################################################################################')
